c4e4c46c014ffe2af84fe2469d585218f2af2bcb
sarahmds <sarah.santos@escolar.ifrn.edu.br>
2025-03-11 13:43:14 -0300

    Add files via upload

--- Hotel del Luna/comprovante.js
+++ Hotel del Luna/comprovante.js
@@ -8,6 +8,7 @@ if (document.querySelector("form")) {
         const adultos = document.getElementById("adultos").value;
         const checkin = document.getElementById("check-in").value;
         const checkout = document.getElementById("check-out").value;
+        const observacao = document.getElementById("observacao").value;  
 
         localStorage.setItem("nome", nome);
         localStorage.setItem("contato", contato);
@@ -15,6 +16,7 @@ if (document.querySelector("form")) {
         localStorage.setItem("adultos", adultos);
         localStorage.setItem("checkin", checkin);
         localStorage.setItem("checkout", checkout);
+        localStorage.setItem("observacao", observacao);  
 
         window.location.href = "comprovante.html";
     });
@@ -22,19 +24,21 @@ if (document.querySelector("form")) {
 
 if (document.querySelector("main h2")) {
     window.onload = function() {
-        
         const nome = localStorage.getItem("nome");
         const contato = localStorage.getItem("contato");
         const criancas = localStorage.getItem("criancas");
         const adultos = localStorage.getItem("adultos");
         const checkin = localStorage.getItem("checkin");
         const checkout = localStorage.getItem("checkout");
-
+        const observacao = localStorage.getItem("observacao");  
 
         document.getElementById("nome").textContent = nome;
         document.getElementById("random").textContent = Math.floor(Math.random() * 10000); 
         document.getElementById("checkin").textContent = checkin;
         document.getElementById("checkout").textContent = checkout;
         document.getElementById("pessoas").textContent = `Adultos: ${adultos}, Crianças: ${criancas}`;
+        document.getElementById("observacoes").textContent = observacao || '[Sem Observações]'; 
     };
 }
+
+
